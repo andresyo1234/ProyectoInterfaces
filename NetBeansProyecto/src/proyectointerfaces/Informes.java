bada46fbc22b46481659ea9d9483e3fbd691c3ca
Daniel Espinosa Mauri <92428065+DaniEspinosa@users.noreply.github.com>
2023-01-26 13:03:43 +0100

    cambios

--- NetBeansProyecto/src/proyectointerfaces/Informes.java
+++ NetBeansProyecto/src/proyectointerfaces/Informes.java
@@ -101,6 +101,7 @@ public class Informes extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
 
         jButton6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jButton6.setText("Crear");
@@ -302,10 +303,17 @@ public class Informes extends javax.swing.JFrame {
             .addComponent(BotonPaginaCartera, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        jButton3.setText("Report 1");
+        jButton3.setText("INFORME MEDIA DE CONTACTOS POR USUARIOS");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
+            }
+        });
+
+        jButton4.setText("INFORME TRANSACCIONES DE LA CUENTA");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
             }
         });
 
@@ -315,16 +323,20 @@ public class Informes extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(44, 44, 44)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(73, 73, 73)
-                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 501, Short.MAX_VALUE))
         );
 
@@ -419,17 +431,33 @@ public class Informes extends javax.swing.JFrame {
         try{
         Connection con = Conexion.getConexion();
             System.out.println("0");
-        String reportPath = "C:\\Projects\\ProyectoInterfaces\\Informes\\Informe1.jasper";
+        String reportPath = "C:\\ProyectoInterfaces\\Informes\\Informe1.jasper";
         System.out.println("1");
-        JasperReport jr = JasperCompileManager.compileReport(reportPath);
+        //JasperReport jr = JasperCompileManager.compileReport(reportPath);
         System.out.println("2");
-        JasperPrint jp = JasperFillManager.fillReport(jr,null, con);
+        JasperPrint jp = JasperFillManager.fillReport(reportPath,null, con);
         System.out.println("3");
-        JasperViewer.viewReport(jp);
+        JasperViewer.viewReport(jp,false);
         System.out.println("4");
         con.close();
         }catch(Exception ex){ex.printStackTrace();}
     }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        try{
+        Connection con = Conexion.getConexion();
+            System.out.println("0");
+        String reportPath = "C:\\ProyectoInterfaces\\Informes\\Informe3.jasper";
+        System.out.println("1");
+        //JasperReport jr = JasperCompileManager.compileReport(reportPath);
+        System.out.println("2");
+        JasperPrint jp = JasperFillManager.fillReport(reportPath,null, con);
+        System.out.println("3");
+        JasperViewer.viewReport(jp,false);
+        System.out.println("4");
+        con.close();
+        }catch(Exception ex){ex.printStackTrace();}
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -477,6 +505,7 @@ public class Informes extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton6;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JFrame jFrame1;
